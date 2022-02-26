7ad8efadfd9040431872c9480471d469e54ac361
flxhn <flxhn@DESKTOP-DGK3GJV>
2022-02-26 12:16:00 +0100

    cambio1 cambio1

--- prueba1/src/cambio1/cambio1.java
+++ prueba1/src/cambio1/cambio1.java
@@ -3,7 +3,7 @@ package cambio1;
 public class cambio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("rferpoferokgerogkjer kerg joeij ");
 
 	}
 
